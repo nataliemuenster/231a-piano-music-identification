7d662a8ce64e399bca9e3c2bac0698c612caaddf
Natalie Muenster <ncm000@stanford.edu>
2017-05-29 14:07:59 -0700

    Added video2 images, starting on fitting lines with Hough trasformation from a single sobel image.

--- image_extraction.py
+++ image_extraction.py
@@ -14,9 +14,9 @@ import matplotlib.patches as patches
 def parse_video(video):
 	print "Parse_video:", video
 	return
-	#In command line I used: "ffmpeg  -r 30 -i ../videos/video_1.mp4 -qscale:v 2 -f image2 video_1-%04d.jpg"
+	#In command line I used: "ffmpeg  -r 30 -i ./videos/video_2.mp4 -qscale:v 2 -f image2 video_2-%04d.jpg"
 
-ffmpeg  -r 30 -i ../video_2.mp4 -qscale:v 2 -f image2 video_2-%04d.jpg
+    #not quite the same: ffmpeg  -r 30 -i ../video_2.mp4 -qscale:v 2 -f image2 video_2-%04d.jpg
 
     #We should definitely look into this to crop https://video.stackexchange.com/questions/4563/how-can-i-crop-a-video-with-ffmpeg
 
@@ -50,119 +50,3 @@ def sobel(img): #one at a time? or do all at once?
 	#cv2.imshow("cropped", crop_img)
 	#cv2.waitKey(0)
 	return dst
-
-
-def compute_gradient(im):
-    print "im.shape", im.shape
-    H = im.shape[0]
-    W = im.shape[1]
-
-    xgrad = np.zeros((H-2, W-2))
-    ygrad = np.zeros((H-2, W-2))
-    
-    xgrad = im[1:-1, :-2] - im[1:-1, 2:]
-    ygrad = im[:-2, 1:-1] - im[2:, 1:-1]
-    
-    angles = np.arctan2(ygrad, xgrad)
-    angles[angles < 0] += math.pi
-    angles = np.degrees(angles)
-    magnitudes = np.sqrt(xgrad ** 2 + ygrad ** 2)
-    return angles, magnitudes
-
-
-def generate_histogram(angles, magnitudes, nbins = 9):
-    print angles.shape
-    histogram = np.zeros(nbins)
-    
-    
-    bin_size = float(180 / nbins)
-    
-    # iterate over the pixels
-    for h in xrange(angles.shape[0]):
-        for w in xrange(angles.shape[1]):
-            ang = angles[h,w]
-            mag = magnitudes[h,w]
-            
-            print angles.shape
-            print ang.shape
-            if ang >= 180:
-                ang = ang - 180
-            
-            # interpolate the votes
-            lower_idx = int(ang / bin_size) - 1
-            upper_idx = lower_idx + 1
-            
-            lower_ang = lower_idx * bin_size + 90/nbins
-            upper_ang = upper_idx * bin_size + 90/nbins
-            
-            # Account for edge case
-            if upper_idx >= nbins:
-                upper_idx = 0
-            if lower_idx < 0:
-                lower_idx = nbins - 1
-            
-            lower_diff= abs(ang - lower_ang)
-            upper_diff = abs(ang - upper_ang)
-            lower_percent = upper_diff/ bin_size
-            upper_percent = lower_diff/ bin_size
-            histogram[lower_idx] += lower_percent * mag
-            histogram[upper_idx] += upper_percent * mag
-    
-    return histogram
-
-
-
-def compute_hog_features(im, pixels_in_cell, cells_in_block, nbins):
-    height = im.shape[0] - 2
-    width = im.shape[1] - 2
-    
-    angles, magnitudes = compute_gradient(im)
-    
-    total_cells_in_block = cells_in_block * pixels_in_cell
-    stride = total_cells_in_block / 2
-    features = np.zeros((int(math.floor(height / stride)) - 1,
-                         int(math.floor(width / stride)) - 1,
-                         nbins * cells_in_block * cells_in_block))
-        
-                         # iterate over the blocks, 50% overlap
-    for w in xrange(0, width - total_cells_in_block, stride):
-        for h in xrange(0, height - total_cells_in_block, stride):
-            block_features = np.zeros((cells_in_block, cells_in_block,  nbins))
-            block_magnitude = magnitudes[h:h+total_cells_in_block, w:w+total_cells_in_block]
-            block_angle = angles[h:h+total_cells_in_block, w:w+total_cells_in_block]
-            #  iterate over the cells
-            for i in xrange(cells_in_block):
-                for j in xrange(cells_in_block):
-                    cell_magnitudes = block_magnitude[i * pixels_in_cell:(i+1)
-                                        * pixels_in_cell,
-                                        j*pixels_in_cell:(j+1)*pixels_in_cell]
-                    cell_angles = block_angle[i * pixels_in_cell:(i+1) * pixels_in_cell,
-                                        j*pixels_in_cell:(j+1)*pixels_in_cell]
-                    block_features[i,j,:] = generate_histogram(cell_angles, cell_magnitudes, nbins)
-                                                                 
-            block_features = block_features.flatten()
-            block_features = block_features \
-                / np.sqrt(np.linalg.norm(block_features) ** 2 + .01)
-            features[int(math.ceil(h/(stride))),
-            int(math.ceil(w/(stride))),:] = block_features
-                                                                         
-    return features
-
-# Displays the HoG features next to the original image
-def plot_img_with_bbox(im, bbox, title_text = None):
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    for i in range(bbox.shape[0]):
-        ax.add_patch(
-            patches.Rectangle(
-                (bbox[i,0], bbox[i,1]),
-                bbox[i,2],
-                bbox[i,3],
-                fill=False,
-                edgecolor='red'
-            )
-        )
-    plt.imshow(im, 'gray')
-    if title_text is not None:
-        plt.title(title_text)
-
--- main.py
+++ main.py
@@ -9,18 +9,19 @@ videos_dir = "./data/videos"
 max_RGB_value = 255
 
 #url1 = "https://www.youtube.com/watch?v=9Pk0R8OWg0k"
-video_path = "./data/videos/video_1.mp4"
+'''video_path = "./data/videos/video_1.mp4"
 images_dir = "./data/video_1_images"
 sobel_img_dir = "./data/sobel_images_1"
-base_img_name = "video_1-0122_sobel.jpg"
-
+base_img_name = "video_1-0122.jpg"
+'''
 
 #url2 = "https://www.youtube.com/watch?v=CQTim0KdILE"
+video_name = "video_2" #User can change this for whichever video they want
 video_path = "./data/videos/video_2.mp4"
 images_dir = "./data/video_2_images"
 sobel_img_dir = "./data/sobel_images_2"
-base_img_name = "video_2-0122_sobel.jpg"
-video_name = "video_2" #User can change this for whichever video they want
+base_img_name = "video_2-0001.jpg"
+
 
 if __name__ == '__main__':
 
@@ -32,19 +33,22 @@ if __name__ == '__main__':
     #videos = os.listdir(videos_dir)
     #video = video_name + ".mp4" #do we need to "open" this video at all?
     
-    #extract images from frames
+    #extract images from frames. Doesn't actually do anything rn...
     frames = image_extraction.parse_video(video_path)
     
 
     #kernel = np.ones((5,5),np.uint8) #for dilation/erosion to fill in gaps, used for masking
 
     #get the sobel of just the baseline image (no hands) to get lines using Sobel then Hough transform
-    base_img_name = "video_1-0122_sobel.jpg"
+    '''base_img_name = "video_1-0122.jpg"
     base_img = cv2.imread(os.path.join(images_dir,base_img_name))
     base_img = base_img.astype(np.uint8)
     sobel_img = image_extraction.sobel(base_img)
     sobel_img = cv2.cvtColor(sobel_img, cv2.COLOR_RGB2GRAY) #convert to greyscale to applythresholding to binarize image
 
+    cv2.imshow("sobel", sobel_img)
+ 	cv2.waitKey(0)
+ 	'''
 
     #loops through all the images in the video directory
     '''image_list = os.listdir(images_dir)
@@ -59,48 +63,6 @@ if __name__ == '__main__':
                 images.append(img)
     '''
 
-#Hey Natalie! The following code is my attempts to try to crop along the lines but I'm getting really stuck because honestly figuring how to crop seems like a project in itself. Idk if we should just manually pick a region of the image as a subset of the image matrix
-    """
-                for gray in cv2.split(img):
-                    print "gray", gray
-                        
-                        
-                #dilated = cv2.dilate(src = gray, kernel = kernel, anchor = (-1,-1))
-        
-                #blured = cv2.medianBlur(dilated, 7)
-            
-                # Shrinking followed by expanding can be used for removing isolated noise pixels
-                # another way to think of it is "enlarging the background"
-                # http://www.cs.umb.edu/~marc/cs675/cvs09-12.pdf
-                #small = cv2.pyrDown(blured, dstsize = (width / 2, height / 2))
-                # oversized = cv2.pyrUp(small, dstsize = (width, height))
-                """
-                name = img_name.split(".")[0]
-            #cv2.imshow("image:", img)
-            #cv2.imshow("sobel", images[333])
-            
-            #THIS IS ME TRYING TO MANUALLY CROP but I think we can also do this to the entire video when we're parsing (see image_extraction)
 
-                img = img[80:16+(img.shape[0]/2), :]
-                
-                cv2.imwrite(os.path.join(sobel_img_dir, name + "_sobel.jpg"), img)
-                
-                
-        
-        #The following code is me playinga round with the effect of dilating and eroding the sobel image to see if I can get the lines to be more crip in order to crop
-        """
-        if img_name == "video_1-0122.jpg":
-            im = imread(os.path.join(sobel_img_dir, name + "_sobel.jpg"), 'L').astype(np.uint8)
-
-	#http://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_morphological_ops/py_morphological_ops.html
-            im_eroded = cv2.erode(im,kernel,iterations = 1)
-            #im_dilated = cv2.dilate(im_eroded,kernel,iterations = 1)
-            cv2.imwrite(os.path.join(sobel_img_dir, name + "_E_AND_D.jpg"), img)
-
-
-
-            #cv2.waitKey(0)
-            break
-    """
 
     print "done with main"
