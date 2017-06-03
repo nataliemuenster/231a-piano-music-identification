0f6a62fab0e610be4320b6b64622394c7674fb26
Sarah Radzihovsky <sradzi13@stanford.edu>
2017-06-03 11:57:42 -0700

    trying to introduce user input

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ import preprocess
 import key_detection
 from scipy.misc import imread
 import cv2
-print cv2.__version__
+import urllib
+from win32api import GetSystemMetrics
+
+
 
 videos_dir = "./data/videos"
 max_RGB_value = 255
@@ -25,11 +28,14 @@ base_img_name = "video_2-0001.jpg"
 
 
 if __name__ == '__main__':
-
+    
+    corners, start_key = getUserInput()
+    """
     video_path = "./data/videos/" + video_name + ".mp4"
     images_dir = "./data/" + video_name + "_images"
     if not os.path.exists(images_dir):
         os.mkdir(images_dir)
+    """
     
     #extract images from frames. Doesn't actually do anything rn...
     #frames = image_extraction.parse_video(video_path)
@@ -43,7 +49,10 @@ if __name__ == '__main__':
     #get the sobel of just the baseline image (no hands) to get lines between keys from Hough transform (Right now, key_lines is all lines returned by Hough)
     key_lines = preprocess.getLines(base_img_rectified) #will use base_img_rectified once i finish rectify
     """
-
+    
+    get_corners(img)
+    
+    """
     start_key = "B"
 
     binary_rectified = cv2.imread("./video_2-0001_binary_no_sobel.jpg") #a static variable above main
@@ -53,6 +62,7 @@ if __name__ == '__main__':
     [whiteKeys, numWhiteKeys, blackKeys, numBlackKeys, white_notes, black_notes] = key_detection.detect_keys(binary_rectified, binary_rectified_sobel, start_key)
     
     print whiteKeys, numWhiteKeys, blackKeys, numBlackKeys, white_notes, black_notes
+    """
 
     #loops through all the images in the video directory
     '''image_list = os.listdir(images_dir)
@@ -70,3 +80,56 @@ if __name__ == '__main__':
 
 
     print "done with main"
+
+
+
+"""
+def getUserInput
+    print Hello! In order to process your video, we require a couple pieces of information about your video.
+    top_right = input('Pixel coordinates of the top right corner (in the format [x,y] with no spaces): ')
+    top_right = input('Pixel coordinates of the top right corner (in the format [x,y] with no spaces): ')
+    top_right = input('Pixel coordinates of the top right corner (in the format [x,y] with no spaces): ')
+    top_right = input('Pixel coordinates of the top right corner (in the format [x,y] with no spaces): ')
+
+"""
+
+
+
+#the [x, y] for each right-click event will be stored here
+right_clicks = list()
+
+#this function will be called whenever the mouse is right-clicked
+def mouse_callback(event, x, y, flags, params):
+    
+    #right-click event value is 2
+    if event == 2:
+        global right_clicks
+        
+        #store the coordinates of the right-click event
+        right_clicks.append([x, y])
+        
+        #this just verifies that the mouse data is being collected
+        #you probably want to remove this later
+        print right_clicks
+
+def get_corners(img):
+    width = GetSystemMetrics(0)
+    height = GetSystemMetrics(1)
+    scale_width = 640 / img.shape[1]
+    scale_height = 480 / img.shape[0]
+    scale = min(scale_width, scale_height)
+    window_width = int(img.shape[1] * scale)
+    window_height = int(img.shape[0] * scale)
+    cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('image', window_width, window_height)
+    
+    #set mouse callback function for window
+    cv2.setMouseCallback('image', mouse_callback)
+    
+    cv2.imshow('image', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+
+
